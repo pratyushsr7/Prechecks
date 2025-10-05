1d02438993fbeb9c766f9847c3b1179f5744e2c1
Pratyush Srivastava <101868959+pratyushsr7@users.noreply.github.com>
2025-10-05 11:01:03 +0000

    Added Streamlit app code

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+import pandas as pd 
+import snowflake.connector
+
+def prechecks_app():
+    snowflake_connection = snowflake.connector.connect(
+        host = 'slb78176.innova3.us-east-1.a.p1.satoricyber.net',
+        account = 'slb78176', 
+        user = 'CSH_AUTOMATION_USER', 
+        password = 'dUstaxamezif6ajef4Is',
+        role = 'DATAENGINEER',
+        warehouse = 'CSHNATIONALPRODWAREHOUSE',
+        database = 'DAP'
+    )
+    snowflake_cursor = snowflake_connection.cursor()
+
+    query = 'select * from l1.dap_attribution_prechecks_results'
+
+    df = pd.read_sql(query, snowflake_connection)
+    st.title("Pre-Execution Checks Results")
+    st.dataframe(df)
+
+    # Optional 
+    columns = st.multiselect("Filter by columns")
+    if columns:
+        filtered_df = df[columns]
+
+if __name__ == "__main__":
+    prechecks_app()
